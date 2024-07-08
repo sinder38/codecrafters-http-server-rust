f251f02bd17dd4bca699bfbb2b56f76e5f212e89
sinder <smirnov.sinder@gmail.com>
2024-07-08 11:31:36 +0300

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::io::Write;
-use std::net::TcpListener;
+mod parser;
+use parser::HttpRequest;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,11 +14,32 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+                handle_connection(stream);
+
             }
+
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8_lossy(&buffer[..]);
+    let http_request = HttpRequest::parse(request);
+    match http_request {
+        Ok(request)=>{
+            println!("{:#?}",request);
+            if request.target == "/index.html" || request.target == "/" {
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+            } else {
+                stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
+            }
+        },
+        Err(_) => {
+            stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
         }
     }
 }
---
+++ src/parser.rs
@@ -0,0 +1,83 @@
+use std::collections::HashMap;
+use std::io::BufRead;
+
+#[derive(Debug, PartialEq)]
+pub struct HttpRequest {
+   pub method: String,
+    pub target: String,
+    pub version: String, //TODO maybe it should be an integer
+    pub headers: HashMap<String, String>,
+    pub body: String,
+}
+
+#[derive(Debug)]
+pub enum HttpRequestError {
+    MissingRequestLine,
+    MissingMethod,
+    MissingPath,
+    MissingVersion,
+    InvalidHeader,
+}
+
+impl HttpRequest {
+    pub fn parse<T>(request: T) -> Result<HttpRequest, HttpRequestError>
+    where T: AsRef<str>{
+        let mut lines = request.as_ref().lines();
+
+        // Parse request line
+        let request_line = lines.next().ok_or(HttpRequestError::MissingRequestLine)?;
+        let mut parts = request_line.split(" ");
+        let method = parts.next().ok_or(HttpRequestError::MissingMethod)?;
+        let target = parts.next().ok_or(HttpRequestError::MissingPath)?;
+        let version = parts.next().ok_or(HttpRequestError::MissingVersion)?;
+
+        // Parse headers
+        let mut headers = HashMap::new();
+        for line in &mut lines {
+            let line = line.trim();
+            if line.is_empty() {
+                break;
+            }
+            let mut header_parts = line.splitn(2, ':');
+            headers.insert(header_parts.next().ok_or(HttpRequestError::InvalidHeader)?.trim().to_string(),
+                           header_parts.next().ok_or(HttpRequestError::InvalidHeader)?.trim().to_string());
+        }
+
+        // Parse body
+        let body : String = lines.collect();
+
+        Ok(HttpRequest {
+            method: method.to_string(),
+            target: target.to_string(),
+            version: version.to_string(),
+            headers,
+            body,
+        })
+    }
+}
+
+
+#[test]
+fn test1() {
+    let mut headers = HashMap::new();
+    headers.insert("User-Agent".to_string(), "curl/7.64.1".to_string());
+    headers.insert("Host".to_string(), "localhost:4221".to_string());
+    headers.insert("Accept".to_string(), "*/*".to_string());
+    let desired_request = HttpRequest {
+        method: "GET".to_string(),
+        target: "/index.html".to_string(),
+        version: "HTTP/1.1".to_string(),
+        headers,
+        body: "body".to_string(),
+    };
+
+    let request = "GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\nbody";
+    let parsed_request = HttpRequest::parse(request);
+    match parsed_request {
+        Ok(request) => {
+            println!("{:#?}", request);
+            assert_eq!(request,desired_request);
+        },
+        Err(e) => eprintln!("Failed to parse HTTP request: {:?}", e),
+    }
+}
