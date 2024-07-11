b78db69dcc7cad4a8a2b4074aaf0da5bef800b5d
sinder <smirnov.sinder@gmail.com>
2024-07-11 08:37:37 +0300

    Revert "added async runtime, moved from std to tokio"
    This reverts commit 3376830042be8ad6474e1ca1b8115285010659b6.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
 mod http_response;
 mod parser;
 
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 use std::ops::Deref;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
 
 use crate::http_response::{
     HttpResponse, HttpResponseStatus, PLAIN_TEXT_CONTENT_TYPE, USER_AGENT_KEY,
 };
 use crate::parser::HttpRequest;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+fn main() {
     println!("Logs:");
 
-    let listener = TcpListener::bind("127.0.0.1:4221").await?;
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    loop {
-        // Accept a new socket
-        let (socket, addr) = listener.accept().await?;
-        println!("New connection: {}", addr);
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("accepted new connection");
+                handle_connection(stream);
+            }
 
-        // Spawn a new task to handle the connection
-        tokio::spawn(async move {
-            handle_connection(socket).await;
-        });
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).await.unwrap(); // TODO remove this unwrap
-
+    stream.read(&mut buffer).unwrap();
     let binding = String::from_utf8_lossy(&buffer[..]);
-    let request = binding.deref(); // Not sure if this is the best way to do this. maybe should just clone
+    let request = binding.deref(); // Not sure if this is the best way to do this
     let http_request = HttpRequest::from(request);
     match http_request {
         Ok(r) => {
             println!("{:#?}", r);
             match r.method {
                 "GET" => {
-                    stream.write_all(&process_get(r)).await.unwrap();
+                    stream.write_all(&process_get(r)).unwrap();
                 }
                 _ => {
                     panic!("Unexpected method")
@@ -48,12 +47,7 @@ async fn handle_connection(mut stream: TcpStream) {
             }
         }
         Err(_) => {
-            stream
-                .write_all(
-                    &HttpResponse::new(HttpResponseStatus::NotFound, None, None, None).as_bytes(),
-                )
-                .await
-                .unwrap();
+            stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
         }
     }
 }
@@ -74,6 +68,7 @@ fn process_get(http_request: HttpRequest) -> Vec<u8> {
                     // b"HTTP/1.1 404 Not Found\r\n\r\n"
                     HttpResponse::new(HttpResponseStatus::NotFound, None, None, None).as_bytes()
                 }
+
             }
         }
         None | Some(_) => {
